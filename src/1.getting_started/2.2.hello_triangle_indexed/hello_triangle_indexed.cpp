9d3934a9862555f3f7e0c136669b129c4891a787
xiaowei.guan <xiaowei.guan@samsung.com>
2024-06-13 12:59:47 +0800

    Add indexed triangle

--- src/1.getting_started/2.2.hello_triangle_indexed/hello_triangle_indexed.cpp
+++ src/1.getting_started/2.2.hello_triangle_indexed/hello_triangle_indexed.cpp
@@ -104,20 +104,34 @@ int main(int, char **)
   // set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f, // left
-      0.5f, -0.5f, 0.0f,  // right
-      0.0f, 0.5f, 0.0f    // top
+      0.5f, 0.5f, 0.0f,   // 右上角
+      0.5f, -0.5f, 0.0f,  // 右下角
+      -0.5f, -0.5f, 0.0f, // 左下角
+      -0.5f, 0.5f, 0.0f   // 左上角
   };
 
-  unsigned int VBO, VAO;
+  unsigned int indices[] = {
+      // 注意索引从0开始!
+      // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+      // 这样可以由下标代表顶点组合成矩形
+
+      0, 1, 3, // 第一个三角形
+      1, 2, 3  // 第二个三角形
+  };
+
+  unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
   // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
   // then configure vertex attributes(s).
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
@@ -146,7 +160,8 @@ int main(int, char **)
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
     // etc.)
@@ -157,6 +172,7 @@ int main(int, char **)
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
   glDeleteProgram(shaderProgram);
 
   // glfw: terminate, clearing all previously allocated GLFW resources.
